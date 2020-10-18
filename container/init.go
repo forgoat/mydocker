378621635a0018a1840c97d7dbd997f282ba8db9
forgoat <20420162201666@stu.xmu.edu.cn>
2020-10-19 03:33:27 +0800

    cancel mount /proc

--- container/init.go
+++ container/init.go
@@ -9,13 +9,13 @@ import (
 
 func RunContainerInitProcess(cmd string, args []string) error {
 	log.Infof("command init %s", cmd)
-	/*	if err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""); err != nil {
+/*	if err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""); err != nil {
 		fmt.Println("Error1 ", err)
-	}*/
+	}
 	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
 	if err := syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), ""); err != nil {
 		fmt.Println("Error2 ", err)
-	}
+	}*/
 	argv := []string{cmd}
 
 	if err := syscall.Exec(cmd, argv, os.Environ()); err != nil {
